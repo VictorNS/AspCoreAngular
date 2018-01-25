ddf0e7c4eed85e0bf866bfcdf04d899d8bb00bf1
VictorNS <VictorNS@mail.ru>
2018-01-25 18:41:16 +0700

    ASP.NET application

---
+++ AspCoreAngular/Controllers/PingController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace AspCoreAngular.Controllers
+{
+	[Route("api/[controller]")]
+	public class PingController : Controller
+	{
+		// GET: api/<controller>
+		[HttpGet]
+		public IEnumerable<string> Get()
+		{
+			return new string[] { "Hello", "World" };
+		}
+	}
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # AspCoreAngular
 Angular application hosted in ASP.NET Core
+
+It's just an example for the article "How to build an Angular Application with ASP.NET Core in Visual Studio 2017, visualized" (https://medium.com/@levifuller/building-an-angular-application-with-asp-net-core-in-visual-studio-2017-visualized-f4b163830eaa)
